294c024773d2df4446b62dfaea02270af9433dd5
Edip Hazuri <edip@medip.dev>
2025-08-21 18:29:58 +0300

    feat: 4 zone test

--- hp-wmi.c
+++ hp-wmi.c
@@ -1546,8 +1546,8 @@ static int __init hp_kbd_rgb_setup(void)
 		case HP_KEYBOARD_TYPE_FOURZONE_WITHOUT_NUMPAD:
 			pr_info("keyboard type %d, four zone RGB keyboard support\n",
 				keyboard_type);
-			// return hp_mc_leds_register(4); WIP
-			return -ENODEV;
+			return hp_mc_leds_register(4); WIP
+			// return -ENODEV;
 		case HP_KEYBOARD_TYPE_RGB_PER_KEY:
 			pr_info("per key-RGB keyboard detected but not supported yet\n");
 			return -ENODEV;
