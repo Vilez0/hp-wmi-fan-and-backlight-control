42fc8b37ad9bcbc783838825e7a1e544f5d03fad
Edip Hazuri <edip@medip.dev>
2025-09-01 09:24:14 +0300

    fix: mark another 16-s0xxx (8bd5) for victus_s thermal profile

--- hp-wmi.c
+++ hp-wmi.c
@@ -99,7 +99,7 @@ static const char * const victus_thermal_profile_boards[] = {
 
 /* DMI Board names of Victus 16-r and Victus 16-s laptops */
 static const char * const victus_s_thermal_profile_boards[] = {
-	"8BBE", "8BD4", "8C99", "8C9C"
+	"8BBE", "8BD4", "8BD5", "8C99", "8C9C"
 };
 
 enum hp_wmi_radio {
